61ffb426db0afae21c7078cfefe89b75c8a07bc6
Nobody-IArc <utk3970@naver.com>
2025-03-23 16:17:02 +0900

    Nest 내장 multer 모듈을 이용한 파일 업로드 기능

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload-file')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file.buffer.toString('utf-8'));
+    return 'Upload File';
   }
 }
